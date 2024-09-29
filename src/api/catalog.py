c1eb60e17d17648aae4a28d1bb203f09d915ea3d
Kevin Rutledge <kevin.rutledge.89@gmail.com>
2024-09-29 11:08:17 -0700

    adding persistence v1.14 - logging feature implemented from v1.13, changed catalog to sell 1 instead of all at 50 gold

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -25,7 +25,7 @@ def get_catalog():
             {
                 "sku": "GREEN_POTION_0",
                 "name": "green potion",
-                "quantity": num_green_potions,
+                "quantity": 1,
                 "price": 50,
                 "potion_type": [0, 100, 0, 0],
             }
