b0c0638e470640acaf18ee63e10a17a2287fd1a5
Kevin Rutledge <kevin.rutledge.89@gmail.com>
2024-11-12 02:20:23 -0800

    feat: testing

--- src/database.py
+++ src/database.py
@@ -2,9 +2,36 @@ import os
 import dotenv
 from sqlalchemy import create_engine
 
-def database_connection_url():
-    dotenv.load_dotenv()
+def create_engine_with_config(testing: bool = False):
+    """Create database engine with appropriate configuration"""
+    if testing:
+        return create_engine(
+            "sqlite:///:memory:",
+            connect_args={"check_same_thread": False},
+            isolation_level="SERIALIZABLE",
+            pool_pre_ping=True
+        )
+    else:
+        dotenv.load_dotenv()
+        postgres_url = os.environ.get("POSTGRES_URI")
+        return create_engine(
+            postgres_url,
+            isolation_level="SERIALIZABLE",
+            pool_pre_ping=True
+        )
 
-    return os.environ.get("POSTGRES_URI")
+# Global engine
+engine = create_engine_with_config(testing=False)
 
-engine = create_engine(database_connection_url(), pool_pre_ping=True)
+# Switch engine
+def use_test_db():
+    """Switch to SQLite test database"""
+    global engine
+    engine = create_engine_with_config(testing=True)
+    return engine
+
+def use_prod_db():
+    """Switch back to PostgreSQL database"""
+    global engine
+    engine = create_engine_with_config(testing=False)
+    return engine
